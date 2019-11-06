59b1502e2fe698ade18474807a7f81671cbf28da
vermin <vermin@techie.com>
2019-11-06 02:06:17 +0100

    cmake lnew location

--- cmake/FindWAZN.cmake
+++ cmake/FindWAZN.cmake
@@ -2,8 +2,8 @@
 # CMake helper for the majority of the cpp-ethereum modules.
 #
 # This module defines
-#     WAZN_XXX_LIBRARIES, the libraries needed to use ethereum.
-#     WAZN_FOUND, If false, do not try to use ethereum.
+#     Wazn_XXX_LIBRARIES, the libraries needed to use ethereum.
+#     Wazn_FOUND, If false, do not try to use ethereum.
 #
 # File addetped from cpp-ethereum
 #
@@ -32,7 +32,7 @@ set(LIBS common;blocks;cryptonote_basic;cryptonote_core;multisig;
 		cryptonote_protocol;daemonizer;mnemonics;epee;lmdb;device;
 		blockchain_db;ringct;wallet;cncrypto;easylogging;version;checkpoints)
 
-set(WAZN_INCLUDE_DIRS "${CPP_WAZN_DIR}")
+set(Wazn_INCLUDE_DIRS "${CPP_WAZN_DIR}")
 
 # if the project is a subset of main cpp-ethereum project
 # use same pattern for variables as Boost uses
@@ -41,24 +41,24 @@ foreach (l ${LIBS})
 
 	string(TOUPPER ${l} L)
 
-	find_library(WAZN_${L}_LIBRARY
+	find_library(Wazn_${L}_LIBRARY
 		NAMES ${l}
 		PATHS ${CMAKE_LIBRARY_PATH}
 		PATH_SUFFIXES "/src/${l}" "/src/" "/external/db_drivers/lib${l}" "/lib" "/src/crypto" "/contrib/epee/src" "/external/easylogging++/"
 		NO_DEFAULT_PATH
 	)
 
-	set(WAZN_${L}_LIBRARIES ${WAZN_${L}_LIBRARY})
+	set(Wazn_${L}_LIBRARIES ${Wazn_${L}_LIBRARY})
 
-	message(STATUS FindWAZN " WAZN_${L}_LIBRARIES ${WAZN_${L}_LIBRARY}")
+	message(STATUS FindWazn " Wazn_${L}_LIBRARIES ${Wazn_${L}_LIBRARY}")
 
 	add_library(${l} STATIC IMPORTED)
-	set_property(TARGET ${l} PROPERTY IMPORTED_LOCATION ${WAZN_${L}_LIBRARIES})
+	set_property(TARGET ${l} PROPERTY IMPORTED_LOCATION ${Wazn_${L}_LIBRARIES})
 
 endforeach()
 
 if (EXISTS ${WAZN_BUILD_DIR}/src/ringct/libringct_basic.a)
-	message(STATUS FindWAZN " found libringct_basic.a")
+	message(STATUS FindWazn " found libringct_basic.a")
 	add_library(ringct_basic STATIC IMPORTED)
 	set_property(TARGET ringct_basic
 			PROPERTY IMPORTED_LOCATION ${WAZN_BUILD_DIR}/src/ringct/libringct_basic.a)
