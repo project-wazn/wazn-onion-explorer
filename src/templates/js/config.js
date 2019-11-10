a18240b91ad28f0e613cbd4544cbbbfbac1c940d
vermin <vermin@techie.com>
2019-11-10 05:35:56 +0100

    nicer :P

--- src/templates/js/config.js
+++ src/templates/js/config.js
@@ -1,7 +1,7 @@
 var config = {
     testnet: false, // this is adjusted page.h if needed. dont need to change manually
     stagenet: false, // this is adjusted page.h if needed. dont need to change manually
-    coinUnitPlaces: 6,
+    coinUnitPlaces: 12,
     txMinConfirms: 10,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in WAZN
     txCoinbaseMinConfirms: 60, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in WAZN
     coinSymbol: 'WAZN',
