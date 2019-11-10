468a4c779aaceb76919544083edd5ea430275737
vermin <vermin@techie.com>
2019-11-10 06:06:02 +0100

    that button was making me crazy

--- src/templates/js/config.js
+++ src/templates/js/config.js
@@ -1,22 +1,22 @@
 var config = {
     testnet: false, // this is adjusted page.h if needed. dont need to change manually
     stagenet: false, // this is adjusted page.h if needed. dont need to change manually
-    coinUnitPlaces: 12,
+    coinUnitPlaces: 0,
     txMinConfirms: 10,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in WAZN
     txCoinbaseMinConfirms: 60, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in WAZN
     coinSymbol: 'WAZN',
     openAliasPrefix: "wazn",
     coinName: 'WAZN',
     coinUriPrefix: 'wazn:',
-    addressPrefix: 0xd1,
-    integratedAddressPrefix: 0x404f,
-    subAddressPrefix: 0x3750,
-    addressPrefixTestnet: 0xd1,
-    integratedAddressPrefixTestnet: 0x404f,
-    subAddressPrefixTestnet: 0x3750,
-    addressPrefixStagenet: 0xd1,
-    integratedAddressPrefixStagenet: 0x404f,
-    subAddressPrefixStagenet: 0x3750,
+    addressPrefix: 0x137130,
+    integratedAddressPrefix: 0x3bab30,
+    subAddressPrefix: 0x2fab30,
+    addressPrefixTestnet: 0x9dd32,
+    integratedAddressPrefixTestnet: 0x5d32,
+    subAddressPrefixTestnet: 0x5eb2,
+    addressPrefixStagenet: 0x81032,
+    integratedAddressPrefixStagenet: 0x5032,
+    subAddressPrefixStagenet: 0x51b2,
     feePerKB: new JSBigInt('20000'),//20^10 - for testnet its not used, as fee is dynamic.
     dustThreshold: new JSBigInt('10000'),//10^10 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
     txChargeRatio: 0.5,
